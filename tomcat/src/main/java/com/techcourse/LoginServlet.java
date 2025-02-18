794b73f11121d3e582a3b6bc8560b858d682f562
ehtjsv2 <ehtjsv2@naver.com>
2025-02-18 17:30:49 +0900

    feat(app): 로그인 세션 구현

--- tomcat/src/main/java/com/techcourse/LoginServlet.java
+++ tomcat/src/main/java/com/techcourse/LoginServlet.java
@@ -1,20 +1,30 @@
 package com.techcourse;
 
 import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
 import org.apache.catalina.servletcontainer.Http11Servlet;
 import org.apache.coyote.FileLoader;
 import org.apache.coyote.http11.StatusCode;
 import org.apache.coyote.http11.request.ContentType;
 import org.apache.coyote.http11.request.Http11Request;
 import org.apache.coyote.http11.response.Http11Response;
+import org.apache.coyote.http11.session.Session;
 
 public class LoginServlet extends Http11Servlet {
 
     @Override
     protected void doGet(Http11Request http11Request, Http11Response http11Response) {
-        http11Response.setStatusCode(StatusCode.OK);
-        http11Response.setContentType(ContentType.HTML);
-        http11Response.setBody(FileLoader.readStaticResourceAllLine("login.html"));
+        Session session = http11Request.getSession(true);
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            getLoginPage(http11Response);
+            return;
+        }
+        InMemoryUserRepository.findByAccount(user.getAccount())
+                .ifPresentOrElse(
+                        (user1) -> redirectIndexPage(http11Response),
+                        () -> getLoginPage(http11Response)
+                );
     }
 
     @Override
@@ -24,14 +34,28 @@ public class LoginServlet extends Http11Servlet {
         InMemoryUserRepository.findByAccount(account)
                 .filter(user -> user.checkPassword(password))
                 .ifPresentOrElse(
-                        (user) -> processLoginSuccess(http11Response),
+                        (user) -> processLoginSuccess(user, http11Request, http11Response),
                         () -> processLoginFail(http11Response)
                 );
     }
 
-    private void processLoginSuccess(Http11Response http11Response) {
+    private void redirectIndexPage(Http11Response http11Response) {
         http11Response.setStatusCode(StatusCode.FOUND);
+        http11Response.setContentType(ContentType.HTML);
         http11Response.sendRedirection("/index.html");
+    }
+
+    private void getLoginPage(Http11Response http11Response) {
+        http11Response.setStatusCode(StatusCode.OK);
+        http11Response.setContentType(ContentType.HTML);
+        http11Response.setBody(FileLoader.readStaticResourceAllLine("login.html"));
+    }
+
+    private void processLoginSuccess(User user, Http11Request http11Request, Http11Response http11Response) {
+        Session session = http11Request.getSession(true);
+        session.setAttribute("user", user);
+        http11Response.addCookie("JSESSIONID", session.getId());
+        redirectIndexPage(http11Response);
     }
 
     private void processLoginFail(Http11Response http11Response) {
--- tomcat/src/test/java/com/techcourse/LoginServletTest.java
+++ tomcat/src/test/java/com/techcourse/LoginServletTest.java
@@ -1,15 +1,20 @@
 package com.techcourse;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import com.techcourse.db.InMemoryUserRepository;
+import com.techcourse.model.User;
+import java.util.UUID;
 import org.apache.coyote.FileLoader;
 import org.apache.coyote.http11.StatusCode;
 import org.apache.coyote.http11.request.ContentType;
 import org.apache.coyote.http11.request.Http11Request;
 import org.apache.coyote.http11.request.RequestBody;
+import org.apache.coyote.http11.request.RequestHeader;
 import org.apache.coyote.http11.request.RequestLine;
 import org.apache.coyote.http11.response.Http11Response;
+import org.apache.coyote.http11.session.Session;
+import org.apache.coyote.http11.session.SessionManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,6 +42,36 @@ class LoginServletTest {
         assertThat(response.getBody()).isEqualTo(FileLoader.readResourceAllLine("login.html"));
     }
 
+    @DisplayName("login get요청시, userSession이 존재하면 index페이지로 리다이렉트한다.")
+    @Test
+    void doGet_return_index_page_when_has_user_session() {
+        // given
+        User testUser = new User(1L, "account", "password", "email");
+        InMemoryUserRepository.save(testUser);
+
+        String uuid = UUID.randomUUID().toString();
+        Session session = new Session(uuid);
+        session.setAttribute("user", testUser);
+        SessionManager.getInstance().add(session);
+
+        Http11Request request = new Http11Request(
+                new RequestLine("GET /login HTTP/1.1 "),
+                new RequestHeader(String.join("\r\n",
+                        "Host: localhost:8080 ",
+                        "Connection: keep-alive ",
+                        "Cookie: JSESSIONID=" + uuid,
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: 25")),
+                null);
+        Http11Response response = Http11Response.createEmptyResponse();
+
+        // when
+        loginServlet.doGet(request, response);
+
+        // then
+        assertThat(response.getHeaderValue("Location")).isEqualTo("/index.html");
+    }
+
     @DisplayName("login Post요청시, 로그인 성공하면 index페이지로 리다이렉션한다.")
     @Test
     void doPostRedirection() {
@@ -58,6 +93,31 @@ class LoginServletTest {
 
         // then
         assertThat(response.getHeaderValue("Location")).isEqualTo("/index.html");
+    }
+
+    @DisplayName("login Post요청시, 로그인 성공하면 JESSIONID를 부여한다.")
+    @Test
+    void doPost_give_JESSIONID() {
+        // given
+        Http11Request request = new Http11Request(
+                new RequestLine("POST /login HTTP/1.1 "),
+                new RequestHeader(String.join("\r\n",
+                        "Host: localhost:8080 ",
+                        "Connection: keep-alive ")),
+                new RequestBody("""
+                        {
+                            "account": "gugu",
+                            "password": "password"
+                        }
+                        """, ContentType.JSON)
+        );
+        Http11Response response = Http11Response.createEmptyResponse();
+
+        // when
+        loginServlet.doPost(request, response);
+
+        // then
+        assertThat(response.getHeaderValue("Set-Cookie")).contains("JSESSIONID");
     }
 
     @DisplayName("login Post요청시, 로그인 성공하면 302 FOUND를 응답한다.")
